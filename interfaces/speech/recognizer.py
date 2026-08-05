2a945b7396950fb6a0c7293187a83e67f5a3c730
rohith-chitturi <chitturirohith333@gmail.com>
2026-08-05 19:55:46 +0530

    fix: explicitly type protocol member for mypy

--- interfaces/speech/recognizer.py
+++ interfaces/speech/recognizer.py
@@ -8,7 +8,7 @@ class SpeechRecognizer(Protocol):
     """
     Interface for speech recognition providers.
     """
-    __version__ = "1.0"
+    __version__: str = "1.0"
     
     def recognize(self, audio_path: str, context: AIContext) -> SpeechRecognitionResult:
         """
